54b5d974576f9a95662919dc59c091815e7bb01c
Brayan Gonzalez Marchena <brayan.gonzalez.marchena@cr.ey.com>
2023-03-25 20:17:20 -0400

    feat: first commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const MerkleTree = require('../utils/MerkleTree');
+const niceList = require('../utils/niceList');
+const verifyProof = require('../utils/verifyProof');
+
+const port = 1225;
+
+const app = express();
+app.use(express.json());
+
+//Initialize Merkle Tree;
+const merkleTree = new MerkleTree(niceList);
+
+// TODO: hardcode a merkle root here representing the whole nice list
+// paste the hex string in here, without the 0x prefix
+const MERKLE_ROOT = merkleTree.getRoot();
+
+app.post('/gift', (req, res) => {
+  // grab the parameters from the front-end here
+  const { name } = req.body;
+
+  // TODO: prove that a name is in the list
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  if (isInTheList) {
+    res.send("You got a toy robot!");
+  }
+  else {
+    res.send("You are not on the list :(");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
